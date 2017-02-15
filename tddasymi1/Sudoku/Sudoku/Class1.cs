3924703e1653118c1f7a7f18134881a076fda435
john nicholas <themrtortoise@gmail.com>
2017-02-15 20:49:30 +0000

    another passing test for completing a row

--- tddasymi1/Sudoku/Sudoku/Class1.cs
+++ tddasymi1/Sudoku/Sudoku/Class1.cs
@@ -10,11 +10,20 @@ namespace Sudoku
     [TestFixture]
     public class Tests
     {
+        [Test]
         public void CompleteARowWithOneMissingNumber()
         {
             string inputRow = "12345678";
             string row = inputRow + "9";
             Assert.That(row, Is.EqualTo("123456789"));
+        }
+
+        [Test]
+        public void CompleteAnotherRowWithOneMissingNumber()
+        {
+            string inputRow = "23456789";
+            string row = "1" + inputRow;
+            Assert.That(row,Is.EqualTo("123456789"));
         }
         
     }
