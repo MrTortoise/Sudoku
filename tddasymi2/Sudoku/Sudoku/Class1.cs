13306b46fafa21424cd4cb425025dd09ba16268e
john nicholas <themrtortoise@gmail.com>
2017-02-15 22:08:21 +0000

    first passing test

--- tddasymi2/Sudoku/Sudoku/Class1.cs
+++ tddasymi2/Sudoku/Sudoku/Class1.cs
@@ -11,9 +11,23 @@ namespace Sudoku
     public class SomeTests
     {
         [TestCase()]
-        public void FailingTest()
+        public void GivenSodokuWithOneMissingValueSolve()
         {
-            Assert.Fail();
+            var goldenMaster = new int[,]
+            {
+                {0, 6, 7, 8, 9, 1, 2, 3, 5},
+                {9, 1, 5, 3, 2, 4, 7, 8, 6},
+                {8, 2, 3, 6, 5, 7, 9, 4, 1},
+                {1, 8, 6, 2, 7, 9, 4, 5, 3},
+                {5, 3, 9, 1, 4, 6, 8, 2, 7},
+                {7, 4, 2, 5, 3, 8, 1, 6, 9},
+                {3, 9, 1, 4, 6, 2, 5, 7, 8},
+                {2, 5, 8, 7, 1, 3, 6, 9, 4},
+                {6, 7, 4, 9, 8, 5, 3, 1, 2}
+            };
+
+            int[,] solvedSodoku = (int[,])goldenMaster.Clone();
+            Assert.That(solvedSodoku, Is.EqualTo(goldenMaster));
         }
     }
 }
