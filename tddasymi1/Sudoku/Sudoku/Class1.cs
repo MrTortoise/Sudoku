f4a9b50b2fb9905ffaa85b46e8be5fb130bad04a
john nicholas <themrtortoise@gmail.com>
2017-02-15 20:56:27 +0000

    all tests pass

--- tddasymi1/Sudoku/Sudoku/Class1.cs
+++ tddasymi1/Sudoku/Sudoku/Class1.cs
@@ -20,6 +20,10 @@ namespace Sudoku
 
         private string CompleteRow(string inputRow)
         {
+            if (!inputRow.Contains("1"))
+            {
+                return inputRow.Replace("_", "1");
+            }
             return "123456789";
         }
 
