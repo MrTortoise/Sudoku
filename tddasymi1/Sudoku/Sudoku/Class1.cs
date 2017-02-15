d48de672b80fe7e172e7f1fd531b4bdf57137058
john nicholas <themrtortoise@gmail.com>
2017-02-15 20:51:51 +0000

    factored out complete row, made tests pass

--- tddasymi1/Sudoku/Sudoku/Class1.cs
+++ tddasymi1/Sudoku/Sudoku/Class1.cs
@@ -14,15 +14,20 @@ namespace Sudoku
         public void CompleteARowWithOneMissingNumber()
         {
             string inputRow = "12345678";
-            string row = inputRow + "9";
+            string row = CompleteRow(inputRow);
             Assert.That(row, Is.EqualTo("123456789"));
+        }
+
+        private string CompleteRow(string inputRow)
+        {
+            return inputRow + "9";
         }
 
         [Test]
         public void CompleteAnotherRowWithOneMissingNumber()
         {
             string inputRow = "23456789";
-            string row = "1" + inputRow;
+            string row = CompleteRow(inputRow);
             Assert.That(row,Is.EqualTo("123456789"));
         }
         
