fa0f979cf9ef59bc709140403da25ac4cfc79953
john nicholas <themrtortoise@gmail.com>
2017-02-15 20:47:45 +0000

    first test

--- tddasymi1/Sudoku/Sudoku/Class1.cs
+++ tddasymi1/Sudoku/Sudoku/Class1.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Sudoku
 {
-    public class Class1
+    [TestFixture]
+    public class Tests
     {
+        public void CompleteARowWithOneMissingNumber()
+        {
+            string inputRow = "12345678";
+            string row = inputRow + "9";
+            Assert.That(row, Is.EqualTo("123456789"));
+        }
+        
     }
 }
