74749d4950eb7cb24f36e504391b98d2083d28a6
john nicholas <themrtortoise@gmail.com>
2017-02-15 22:37:21 +0000

    factored to solve a row

--- tddasymi2/Sudoku/Sudoku/Class1.cs
+++ tddasymi2/Sudoku/Sudoku/Class1.cs
@@ -41,10 +41,48 @@ namespace Sudoku
 
         private int[,] SolveSodoku(int[,] input)
         {
-            var solvedSodoku = input;
-            solvedSodoku[0, 0] = 4;
-            solvedSodoku[0, 1] = 6;
-            return solvedSodoku;
+            var beingSolved = (int[,])input.Clone();
+            List<Tuple<int,int>> pointsToSolve = new List<Tuple<int, int>>();
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i, j] == 0)
+                    {
+                        pointsToSolve.Add(new Tuple<int, int>(i, j));
+                    }
+                }
+            }
+
+            foreach (Tuple<int, int> point in pointsToSolve)
+            {
+                List<int> possibleRowValues = GetPossibleRowValues(input, point.Item1);
+                if (possibleRowValues.Count == 1)
+                {
+                    beingSolved[point.Item1, point.Item2] = possibleRowValues[0];
+                }
+            }
+
+            return beingSolved;
+        }
+
+        private List<int> GetPossibleRowValues(int[,] input, int rowIndex)
+        {
+            int[] row = new int[9];
+            for (int i = 0; i < 9; i++)
+            {
+                row[i] = input[rowIndex, i];
+            }
+
+            List<int> possibleValues = new List<int>();
+            for (int i = 1; i < 10; i++)
+            {
+                if (!row.Contains(i))
+                {
+                    possibleValues.Add(i);
+                }
+            }
+            return possibleValues;
         }
 
         [TestCase()]
